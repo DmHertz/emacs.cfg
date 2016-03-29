3b10cfbb1052e98a2d8214f3be9b84f183695ff5
Dmitry Hertz <dmitryhertz@gmail.com>
2016-03-29 19:24:27 +0600

    modified: conf/global-cfg.el, added fn make-deb-control-description, fn debian-control-description is optimized

--- conf/global-cfg.el
+++ conf/global-cfg.el
@@ -14,25 +14,56 @@
 ;;; archiver, needs for 'windows-nt
 (when (eq 'windows-nt system-type)
   (add-to-list 'exec-path "C:/Program Files/7-Zip"))
-;;;
+;;; -----------------------------------
+(defun arch ()
+  (if (string-equal "x86_64" (substring (shell-command-to-string "arch") 0 -1))
+      "amd64" "x86"))
+
+;; (defun deb-package-root-dir ()
+;;   (if (string-equal "DEBIAN"
+;;                     (file-name-nondirectory
+;;                      (directory-file-name default-directory)))
+;;       (file-name-directory (directory-file-name default-directory))
+;;     (message "You aren't in DEBIAN dir!")))
+
 (defun debian-control-description ()
   (interactive)
-  (let* ((data-str (substring
-                  (shell-command-to-string "pwd")
-                  0 -1))
-       (data-list (car (last (butlast (split-string data-str "/")))))
-       (dirname  (split-string data-list "_"))
-       (pkg-name (car dirname))
-       (pkg-version (cadr dirname))
-       (pkg-arch (caddr dirname)))
-  (insert "Package: " pkg-name "\n"
-          "Version: " pkg-version "\n"
-          "Architecture: " pkg-arch "\n"
-          "Maintainer: " my-name " " my-email "\n"
-          "Depends: \n"
-          "Priority: optional\n"
-          "Description: fixme")))
+  (let* ((data-str (substring default-directory 0 -1))
+         (data-list (car (last (butlast (split-string data-str "/")))))
+         (dirname  (split-string data-list "_"))
+         (pkg-name (car dirname))
+         (pkg-version (cadr dirname))
+         (pkg-arch (caddr dirname)))
+    (insert "Package: " pkg-name "\n"
+            "Version: " pkg-version "\n"
+            "Architecture: " pkg-arch "\n"
+            "Maintainer: " my-name " " my-email "\n"
+            "Depends: \n"
+            "Priority: optional\n"
+            "Description: fixme")))
+
+(defun make-deb-control-description ()
+  (interactive)
+  (let* ((prjnv ;;; project-and-version
+          (split-string
+           (file-name-nondirectory (directory-file-name default-directory))
+           "-"))
+         (debian-dir (concat (car prjnv) "_" (cadr prjnv) "_" (arch) "/DEBIAN")))
+    (make-directory debian-dir)
+    (generate-new-buffer "control")
+    (switch-to-buffer "control")
+    (insert "Package: " (car prjnv) "\n"
+            "Version: " (cadr prjnv) "\n"
+            "Architecture: " (arch) "\n"
+            "Maintainer: " my-name " " my-email "\n"
+            "Depends: \n"
+            "Priority: optional\n"
+            "Description: fixme\n")
+    (backward-char)
+    (write-file (concat debian-dir "/control"))))
 
 (defun find-fish-config ()
   (interactive)
   (find-file "~/.config/fish/config.fish"))
+
+
