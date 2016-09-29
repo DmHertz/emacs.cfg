13e81a7dadff11015b8a5cf85abd01426d37e11f
MetaHertz <dmitryhertz@gmail.com>
2016-09-29 15:54:21 +0700

    init.el fixed

--- init.el
+++ init.el
@@ -2,22 +2,23 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get") ;;; | load el-get
 (setq debug-on-error nil)                           ;;; | 
 ;;; use el-get and download it if not found
-;; (unless (require 'el-get nil 'noerror)
-;;   (with-current-buffer
-;;       (url-retrieve-synchronously
-;;        "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-;;     (goto-char (point-max))
-;;     (eval-print-last-sexp)))
-
-;; using el-get via melpa
 (unless (require 'el-get nil 'noerror)
-  (require 'package)
-  (add-to-list 'package-archives
-               '("melpa" . "http://melpa.org/packages/"))
-  (package-refresh-contents)
-  (package-initialize)
-  (package-install 'el-get)
-  (require 'el-get))
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+;; ;; using el-get via melpa
+;; (unless (require 'el-get nil 'noerror)
+;;   (require 'package)
+;;   (add-to-list 'package-archives
+;;                '("melpa" . "http://melpa.org/packages/"))
+;;   (package-refresh-contents)
+;;   (package-initialize)
+;;   (package-install 'el-get)
+;;   (require 'el-get))
+
 ;;; recipes 
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 
@@ -58,8 +59,8 @@
 ;;; (el-get-bundle johnwalker/pixie-mode)       ;;; | pixie-lang support
 (el-get-bundle clojure-emacs/inf-clojure)   ;;; | inferior clojure
 (el-get-bundle greghendershott/racket-mode) ;;; | racket-lang support
-;;;(el-get-bundle cask/cask)                   ;;; | project management tool for emacs
-;;;(el-get-bundle skeeto/emacsql)
+;;; (el-get-bundle cask/cask)                   ;;; | project management tool for emacs
+;;; (el-get-bundle skeeto/emacsql)              ;;; | improved sql support
 
 ;; parinfer
 ;;(el-get-bundle edpaget/parinfer-mode)   ;;; I don't understood how it use
@@ -109,32 +110,3 @@
 ;; require all the configs automatically
 (seq-doseq (cfg conf-list)
   (require-config cfg))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(browse-url-browser-function (quote browse-url-generic))
- '(cider-cljs-lein-repl
-   "(do (require 'weasel.repl.websocket) 
-    (cemerick.piggieback/cljs-repl
-      (weasel.repl.websocket/repl-env :ip \"127.0.0.1\" :port 9001)))" t)
- '(markdown-command "multimarkdown")
- '(package-selected-packages (quote (rainbow-mode queue)))
- '(racket-images-inline t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(font-lock-comment-delimiter-face ((t (:foreground "#222222" :slant normal))))
- '(linum ((t (:background "#000000" :foreground "#003366"))))
- '(rainbow-delimiters-depth-1-face ((t (:foreground "#FF4C00"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "#FFFF00"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "#FF0000"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "#00FF00"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "#EEEEEE"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "#0066FF"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "#9900FF"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "#00FFFF"))))
- '(rainbow-delimiters-depth-9-face ((t (:foreground "#00CC33")))))
