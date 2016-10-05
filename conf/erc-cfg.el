af3c7591f851c027e2a83b67b9c4f1bf1ef5f4cd
MetaHertz <dmitryhertz@gmail.com>
2016-10-05 16:38:00 +0700

    setq are joined to one big sexp; erc-cfg.el a little bit cleaned.

--- conf/erc-cfg.el
+++ conf/erc-cfg.el
@@ -19,18 +19,45 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
-;; 
-
-;;; Code:
-
-;;; Emacs IRC configuration
-;;; This file is part of the Emacs Dev Kit
+;;  Emacs IRC configuration
 
 (require 'erc)
 (require 'erc-log)
 ;;(require 'erc-spelling)
 (require 'erc-autoaway)
+
+(setq erc-user-full-name "Dmitry Buhaev"        ;; share my real name
+      erc-server-coding-system '(utf-8 . utf-8) ;; utf-8 always and forever
+      erc-autojoin-timing 'ident
+      erc-autojoin-channels-alist (ercchannels "~/.erc/channels")      
+      ;; Interpret mIRC-style color commands in IRC chats
+      erc-interpret-mirc-color t
+      ;; The following are commented out by default, but users of other
+      ;; non-Emacs IRC clients might find them useful.
+      ;; Kill buffers for channels after /part
+      erc-kill-buffer-on-part t
+      ;; Kill buffers for private queries after quitting the server
+      erc-kill-queries-on-quit t
+      ;; Kill buffers for server messages after quitting the server
+      erc-kill-server-buffer-on-quit t
+      ;; open query buffers in the current window
+      erc-query-display 'buffer
+      erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
+                                "324" "329" "332" "333" "353" "477")
+      ;; logging:
+      erc-log-enable t
+      erc-log-file-coding-system 'utf-8
+      erc-log-insert-log-on-open nil
+      erc-log-channels t
+      erc-log-channels-directory "~/.erc/logs/"
+      erc-save-buffer-on-part t
+      erc-hide-timestamps nil
+      erc-save-buffer-on-part t
+
+      ;; autoaway setup
+      erc-auto-discard-away t
+      erc-autoaway-idle-seconds 600
+      erc-autoaway-use-emacs-idle t)
 
 ;; auto identify
 (let ((ercpassfile "~/.ercpass"))
@@ -38,11 +65,10 @@
     (load ercpassfile)
     (require 'erc-services)
     (erc-services-mode 1)
-    (setq erc-prompt-for-nickserv-password nil)
-    (setq erc-nickserv-passwords
+    (setq erc-prompt-for-nickserv-password nil
+          erc-nickserv-passwords
           `((freenode (("MetaHertz" . ,freenode-metahertz)))))))
 
-(setq erc-autojoin-timing 'ident) 
 ;; Join the a couple of interesting channels whenever connecting to Freenode.
 (defun parse-channels-file (filepath)
   "read file, delete comments, add entries to list, remove empties"
@@ -56,44 +82,12 @@
 ;; for test (parse-channels-file "~/.erc/channels")
 ;; for test (macroexpand-1 (ercchannels "~/.erc/channels"))
 
-(setq erc-autojoin-channels-alist (ercchannels "~/.erc/channels"))
-
-;; utf-8 always and forever
-(setq erc-server-coding-system '(utf-8 . utf-8))
-
-;; Interpret mIRC-style color commands in IRC chats
-(setq erc-interpret-mirc-color t)
-
-;; The following are commented out by default, but users of other
-;; non-Emacs IRC clients might find them useful.
-;; Kill buffers for channels after /part
-(setq erc-kill-buffer-on-part t)
-;; Kill buffers for private queries after quitting the server
-(setq erc-kill-queries-on-quit t)
-;; Kill buffers for server messages after quitting the server
-(setq erc-kill-server-buffer-on-quit t)
-
-;; open query buffers in the current window
-(setq erc-query-display 'buffer)
-
 ;; exclude boring stuff from tracking
 (erc-track-mode t)
-(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
-                                "324" "329" "332" "333" "353" "477"))
-
-;; logging:
-(setq erc-log-enable t)
-(setq erc-log-file-coding-system 'utf-8)
-(setq erc-log-insert-log-on-open nil)
-(setq erc-log-channels t)
-(setq erc-log-channels-directory "~/.erc/logs/")
-(setq erc-save-buffer-on-part t)
-(setq erc-hide-timestamps nil)
 
 (if (not (file-exists-p erc-log-channels-directory))
     (mkdir erc-log-channels-directory t))
 
-(setq erc-save-buffer-on-part t)
 (defadvice save-buffers-kill-emacs (before save-logs (arg) activate)
   (save-some-buffers t (lambda () (when (eq major-mode 'erc-mode) t))))
 
@@ -102,18 +96,10 @@
 ;; truncate long irc buffers
 (erc-truncate-mode +1)
 
-;; share my real name
-(setq erc-user-full-name "Dmitry Buhaev")
-
 ;; enable spell checking
 ;;(erc-spelling-mode 1)
 ;; set different dictionaries by different servers/channels
 ;;(setq erc-spelling-dictionaries '(("#emacs" "american")))
-
-;; autoaway setup
-(setq erc-auto-discard-away t)
-(setq erc-autoaway-idle-seconds 600)
-(setq erc-autoaway-use-emacs-idle t)
 
 (defun start-irc ()
   "Connect to IRC."
