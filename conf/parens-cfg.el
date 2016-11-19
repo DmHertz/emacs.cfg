d2f49b3193aef777d4caaa948fe75c41e6e22e30
Dmitry Hertz <dmitryhertz@gmail.com>
2016-11-19 13:08:32 +0700

    conf/rust-cfg.el added; rust-mode added to parens-hooks list

--- conf/parens-cfg.el
+++ conf/parens-cfg.el
@@ -15,7 +15,7 @@
   '(progn
      (add-parens-hooks
       '(lisp common-lisp slime-repl emacs-lisp ielm racket racket-repl clojure cider-repl
-             css html c lua js
+             css html rust c lua js
              eshell gimp markdown))
      ;;; customize
      (sp-pair "(" ")"   :wrap "C-)")
---
+++ conf/rust-cfg.el
@@ -0,0 +1,2 @@
+;; enable cargo-minor-mode by default in rust-mode
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
