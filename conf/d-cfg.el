6642d35673b95f9367be6ded939ae4fff69161af
MetaHertz <dmitryhertz@gmail.com>
2017-04-28 12:55:17 +0700

    dub-packages-srcdir is added In order to flymake could use sources from dub packages.

--- conf/d-cfg.el
+++ conf/d-cfg.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding: t; -*-
+
 ;; this config is borrowed from
 ;; https://www.emacswiki.org/emacs/FlyMakeD
 ;; and partially modified
@@ -5,13 +7,30 @@
 (require 'd-mode)
 (require 'company-dcd)
 
+(defun dub-packages-srcdir (dub-packages-root)
+  (when (file-exists-p dub-packages-root)
+    (let ((src-paths nil))
+      (mapc (lambda (path)
+              (let ((matched (string-match-p "\\(src/\\|source/\\)" path)))
+                (when matched
+                  (push (concat "-I"
+                                (substring path 0 matched)
+                                (car (split-string (substring path matched nil)
+                                                   "/")))
+                        src-paths)))) 
+            (mapcar #'file-name-directory
+                    (directory-files-recursively dub-packages-root
+                                                 "\\(src\\|source\\)" t)))
+      src-paths)))
+
 (defun flymake-d-init ()
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
                      'flymake-create-temp-inplace))
          (local-file (file-relative-name
                       temp-file
                       (file-name-directory buffer-file-name))))
-    (list "dmd" (list "-c" local-file))))
+    (list "dmd" (append (dub-packages-srcdir (expand-file-name "~/.dub/packages"))
+                        (list "-c" local-file)))))
 
 (add-to-list 'flymake-allowed-file-name-masks
              '(".+\\.d$" flymake-d-init 
