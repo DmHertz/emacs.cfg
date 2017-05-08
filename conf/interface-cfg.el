85b471ebaea0ec7f5dc41f697a51ec21b10a27c1
Dmitry Hertz <dmitryhertz@gmail.com>
2017-05-09 00:24:04 +0700

    conf/interface-cfg.el is reorganized
    reinvented the wheel and other useless garbage are removed

--- conf/interface-cfg.el
+++ conf/interface-cfg.el
@@ -1,74 +1,62 @@
 ;; Interface setup ;; -*- lexical-binding: t; -*-
 
-;; set symbol for the border
-(set-display-table-slot standard-display-table
-                        'vertical-border
-                        (make-glyph-code ?│))
-
-;; ;; menu, tool and scroll bars to /dev/null
-(seq-doseq (m '(menu tool scroll))
-  (funcall (append-sym-postfix m "-bar-mode") -1))
-
-;; no splash
-(setq inhibit-splash-screen t)
-;; don't show start up screen, message; and define size
-(setq inhibit-startup-screen t)
-(setq initial-scratch-message nil)
-(line-number-mode 1)
-;; no blinking cursor
-(blink-cursor-mode 0)
-(setq-default cursor-type 'bar)
-;; (set-cursor-color "#ffff00")
-
-(setq scroll-preserve-screen-position t)
-(setq switch-to-buffer-preserve-window-point t)
-
-;; fullscreen
-;; (add-to-list 'default-frame-alist '(fullscreen . fullboth)) ;; broken in 25.1 --with-cairo
-;; ;; Mode bar preferences
-(column-number-mode 1)            ; show column number in mode-line
-(setq display-time-day-and-date t ; display the day and date in the mode line
-      display-time-24hr-format t  ; use 24hr format
-      display-time-interval 10    ; redisplay every ten seconds
-      display-time-default-load-average nil) ; don't display the system load average
-
-(display-time)
-
-;; ;; Enable mouse wheel
-(mouse-wheel-mode 1)
-(setq mouse-wheel-scroll-amount '(1) ; mouse scroll one line at a time
-      mouse-wheel-progressive-speed nil ; don't accelerate scrolling
-      mouse-wheel-follow-mouse t        ; scroll window under mouse
-      scroll-conservatively 10000) ; scroll one line at a time if point moves off-screen
-
-(setq x-select-enable-clipboard t   ; cut and paste to the X clipboard
-      mouse-yank-at-point t)        ; paste at point NOT at cursor
-
-;; highlight line
-(add-hook 'neotree-mode-hook 'hl-line-mode)
-;; ;;; limit line length ;; doesn't work
-;; (require 'whitespace)
-;; (setq whitespace-line-column 80)
-;; (setq whitespace-style '(face lines-tail))
-
-;; Disable r-t-l (arabic, jewish, etc)
-(setq-default bidi-display-reordering nil)
-;; scroll auto
-
-;; Instead «yes» or «no» will be «y» or «n»
-(fset 'yes-or-no-p 'y-or-n-p)
-;; Always show column numbers
-(setq-default column-number-mode t)
-
-;; Display full pathname for files
-(add-hook 'find-file-hooks
-          '(lambda ()
-             (setq mode-line-buffer-identification 'buffer-file-truename)))
-;;(setq transient-mark-mode 1)
-;; 
-(font-lock-mode -1)
-;; Spaces only, without tab chars
-(setq-default indent-tabs-mode nil)
+(setq ;; interface settings
+ ;; disable useless garbage
+ menu-bar-mode nil
+ tool-bar-mode nil
+ scroll-bar-mode nil
+ inhibit-splash-screen t     ;; no start up splash
+ inhibit-startup-screen t    ;; screen
+ initial-scratch-message nil ;; and message
+ ;; mode-line
+ line-number-mode t          ;; line number in mode-line
+ column-number-mode t        ;; column number
+ display-time-day-and-date t ;; display the day and date in the mode line
+ display-time-24hr-format t  ;; use 24hr format
+ display-time-interval 10    ;; redisplay every ten seconds
+ display-time-default-load-average nil ;; don't display the system load average
+ ;;  screen, window, buffer
+ scroll-preserve-screen-position t
+ switch-to-buffer-preserve-window-point t
+  ;; x
+ x-select-enable-clipboard t   ;; cut and paste to the X clipboard
+ ;; mouse
+ mouse-yank-at-point t         ;; paste at point NOT at cursor
+ mouse-wheel-mode t
+ mouse-wheel-scroll-amount '(1)    ;; mouse scroll one line at a time
+ mouse-wheel-progressive-speed nil ;; don't accelerate scrolling
+ mouse-wheel-follow-mouse t        ;; scroll window under mouse
+ scroll-conservatively 10000 ;; scroll one line at a time if point moves off-screen
+ ;; cursor
+ blink-cursor-mode nil
+ cursor-type 'bar
+ ;; font, script
+ font-lock-mode nil
+ indent-tabs-mode nil        ;; spaces only, without tab chars
+ bidi-display-reordering nil ;; disable r-t-l (arabic, jewish, etc)
+ ;; Solarized colorscheme
+ solarized-distinct-fringe-background t     ;; make the fringe stand out from the bg
+ solarized-high-contrast-mode-line t        ;; make the modeline high contrast
+ ;; setq solarized-use-more-italic t        ;; use more italics
+ ;; setq solarized-emphasize-indicators nil ;; use less colors for indicators such
+                                            ;; as git:gutter, flycheck and similar.
+ ;; face font, bg and fg
+ bg-color "#000000"
+ face-font-height 80
+ face-font-step 10
+ face-fonts '("Terminus" "xos4 Terminus" "Terminus (TTF)"
+              "Monospace" "Liberation Mono"
+              "DejaVu Sans Mono" "Inconsolata")
+ default-face-font (case distro
+                     ('arch       (cadr  face-fonts))
+                     ('ubuntu     (car   face-fonts))
+                     ('windows-nt (caddr face-fonts))))
+;; turn on Solarized theme
+(require 'solarized)
+(deftheme solarized-dark
+  "The dark variant of the Solarized color theme")
+(create-solarized-theme 'dark 'solarized-dark)
+(provide-theme 'solarized-dark)
 
 ;; monochrome? seriously? ;; was taken from: Phil Hagelberg / dotfiles
 (eval-after-load 'diff-mode
@@ -80,41 +68,32 @@
 ;;      (set-face-background 'magit-item-highlight "white smoke")
 ;;      (set-face-foreground 'magit-diff-add "green4")
 ;;      (set-face-foreground 'magit-diff-del "red3")))
-;;; ┌──────────────────────────────────┐
-;;; │          color settings          │
-;;; └──────────────────────────────────┘
-;;; Solarized colorscheme
-;;; make the fringe stand out from the background
-(setq solarized-distinct-fringe-background t)
-;;; make the modeline high contrast
-(setq solarized-high-contrast-mode-line t)
-;;; Use more italics
-;;;(setq solarized-use-more-italic t)
-;;; Use less colors for indicators such as git:gutter, flycheck and similar.
-;;;(setq solarized-emphasize-indicators nil)
-
-(require 'solarized)
-(deftheme solarized-dark
-  "The dark variant of the Solarized color theme")
-(create-solarized-theme 'dark 'solarized-dark)
-(provide-theme 'solarized-dark)
 
 ;; transparency '(alpha `active `inactive)
 (let ((active 90)
       (inactive 80))
   (set-frame-parameter (selected-frame) 'alpha (list active inactive))
   (add-to-list 'default-frame-alist (list 'alpha active inactive)))
-;;;face font, bg and fg
-(setq bg-color "#000000"
-      face-font-height 80
-      face-font-step 10
-      face-fonts '("Terminus" "xos4 Terminus" "Terminus (TTF)"
-                   "Monospace" "Liberation Mono"
-                   "DejaVu Sans Mono" "Inconsolata")
-      default-face-font (case distro
-                          ('arch       (cadr  face-fonts))
-                          ('ubuntu     (car   face-fonts))
-                          ('windows-nt (caddr face-fonts))))
+
+;; set symbol for the border
+(set-display-table-slot standard-display-table
+                        'vertical-border
+                        (make-glyph-code ?│))
+
+(add-to-list 'default-frame-alist      ;; fullscreen
+             '(fullscreen . fullboth)) ;; broken in 25.1 --with-cairo
+
+;; Display full pathname for files
+(add-hook 'find-file-hooks
+          '(lambda ()
+             (setq mode-line-buffer-identification 'buffer-file-truename)))
+
+(display-time)
+;; instead «yes» or «no» will be «y» or «n»
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; neotree
+(add-hook 'neotree-mode-hook 'hl-line-mode) ;; highlight line
 
 (defun choose-font (fontname)
   (interactive
@@ -122,36 +101,12 @@
     (completing-read "Choose font: " face-fonts)))
   (set-face-attribute 'default nil :font fontname)) 
 
-(defun apply-font-height (fn)
-  (setq face-font-height
-        (mod (funcall fn face-font-height face-font-step) 360)))
-
-(defun set-font-height (num)
-  (interactive "nEnter height: ")
-  (setq face-font-height num)
-  (set-face-attribute 'default nil :height num))
-
-(defun check-font-height ()
-  "lets to know current height of the font which is using now"
-  (interactive)
-  (message (concat "Font height is " (number-to-string face-font-height))))
-
-(defun -font-height ()
-  (interactive)
-  (apply-font-height #'-)
-  (set-face-attribute 'default nil :height face-font-height))
-
-(defun +font-height ()
-  (interactive)
-  (apply-font-height #'+)
-  (set-face-attribute 'default nil :height face-font-height))
-
 (defmacro set-default-face (body)
   `(if (daemonp)
-      (add-hook 'after-make-frame-functions
-                (lambda (frame)
-                  (select-frame frame)
-                  ,body))
+       (add-hook 'after-make-frame-functions
+                 (lambda (frame)
+                   (select-frame frame)
+                   ,body))
      ,body))
 
 (set-default-face
