36ed7183f0357babbe154fef7d9ea3378e1948c1
Dmitry Hertz <dmitryhertz@gmail.com>
2018-03-08 21:45:35 +0700

    slime-company.el downloading fixed
    Now it selects the right path where to put downloaded slime-company.el

--- conf/slime-cfg.el
+++ conf/slime-cfg.el
@@ -2,19 +2,24 @@
 ;; Set your lisp system and, optionally, some contribs
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 ;; download slime-company package directly
-(let* ((ql-software-base-path "~/quicklisp/dists/quicklisp/software/")
-       (ql-slime-dist-path (car (remove-if-not
-                                 (lambda (s) (string-match "^slime-" s))
-                                 (directory-files ql-software-base-path))))
-       (ql-slime-company-contrib-path "/contrib/slime-company.el")
-       (ql-slime-company-full-path (concatenate 'string
-                                                ql-software-base-path
-                                                ql-slime-dist-path
-                                                ql-slime-company-contrib-path)))
-  (unless (file-exists-p ql-slime-company-full-path)
-    (url-copy-file
-     "https://raw.githubusercontent.com/anwyn/slime-company/master/slime-company.el"
-     ql-slime-company-full-path t)))
+(setq-local ql-software-base-path
+            (expand-file-name "~/quicklisp/dists/quicklisp/software/"))
+(setq-local ql-slime-dist-path
+            (car (sort
+                  (remove-if-not
+                   (lambda (s) (string-match "^slime-" s))
+                   (directory-files ql-software-base-path))
+                  'string-greaterp)))
+(setq-local ql-slime-company-full-path
+            (concatenate 'string
+                         ql-software-base-path
+                         ql-slime-dist-path
+                         "/contrib/slime-company.el"))
+;; download slome-company.el if it doesn't exist in the newest slime-*.* path
+(unless (file-exists-p ql-slime-company-full-path)
+  (url-copy-file
+   "https://raw.githubusercontent.com/anwyn/slime-company/master/slime-company.el"
+   ql-slime-company-full-path t))
 
 (setq slime-lisp-implementations
       (case system-type
