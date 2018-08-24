115d62bf896cbecbd369acf0a19b5dbcd745fd4f
Dmitry Hertz <dmitryhertz@gmail.com>
2018-08-24 22:49:42 +0700

    parens-cfg confix fixed

--- conf/parens-cfg.el
+++ conf/parens-cfg.el
@@ -1,19 +1,30 @@
 ;; -*- lexical-binding: t; -*-
-(defun add-parens-hooks (lst)
+;; smartparens stuff
+;; Set smartparens config
+(require 'smartparens-config)
+;; pmode 1: smartparens
+;; pmode 2: parinfer
+;; cmode 1: rainbow-delimiters
+;; cmode 2: highlight-parentheses
+(defun add-parens-hooks (pmode cmode lst)
   (cond ((null lst)
          (message "parens hooks are done"))
         ((consp lst)
          (let ((mode-name (symbol-name (car lst))))
            (add-hook (intern (concat mode-name "-mode-hook"))
                      (lambda ()
-                       ;; (show-smartparens-mode 1)
-                       ;; (smartparens-strict-mode 1)
-                       (parinfer-mode)
-                       ;; (highlight-parentheses-mode)
-                       (rainbow-delimiters-mode)))
+                       (case pmode
+                         (1 (progn
+                              (show-smartparens-mode 1)
+                              (smartparens-strict-mode 1)))
+                         (2 (parinfer-mode)))
+
+                       (case cmode
+                         (1 (rainbow-delimiters-mode))
+                         (2 (highlight-parentheses-mode)))))
 
            (message (format "%s is done" mode-name))
-           (add-parens-hooks (cdr lst))))))
+           (add-parens-hooks pmode cmode (cdr lst))))))
 
 (setq parinfer-extensions
       '(defaults       ;; should be included.
@@ -24,35 +35,25 @@
          smart-tab     ;; C-b & C-f jump positions and smart shift with tab & S-tab.
          smart-yank))  ;; Yank behavior depend on mode.
 
-(add-parens-hooks '(lisp common-lisp slime-repl emacs-lisp ielm))
-(add-parens-hooks '(scheme racket geiser-repl racket-repl clojure cider-repl))
-(add-parens-hooks '(css html rust c d lua js eshell gimp markdown conf))
+(eval-after-load
+    'smartparens-config
+  '(progn
+     ;; customize
+     (sp-pair "(" ")"   :wrap "C-)")
+     (sp-pair "[" "]"   :wrap "C-]")
+     (sp-pair "\"" "\"" :wrap "C-\"")
+     ;;(sp-pair "{" nil :actions :rem)
+     ;;(sp-pair "'" nil :actions :rem)
+     ;;(sp-pair "`" nil :actions :rem)
 
-;; smartparens stuff
-;; ;; Set smartparens config
-;; (require 'smartparens-config)
+     ;; adds `' as a local pair in emacs-lisp-mode
+     (sp-local-pair 'emacs-lisp-mode "`" "'" :trigger "`")
+     (sp-with-modes '(racket-mode racket-repl-mode scheme-mode lisp-mode emacs-lisp-mode)
+       (sp-local-pair "#|" "|#" :trigger "\\#|"))))
 
-;; (eval-after-load
-;;     'smartparens-config
-;;   '(progn
-;;      (add-parens-hooks
-;;       '(lisp common-lisp slime-repl emacs-lisp ielm
-;;              scheme racket geiser-repl racket-repl clojure cider-repl
-;;              css html rust c d lua js
-;;              eshell gimp markdown conf))
-;;      ;;; customize
-;;      (sp-pair "(" ")"   :wrap "C-)")
-;;      (sp-pair "[" "]"   :wrap "C-]")
-;;      (sp-pair "\"" "\"" :wrap "C-\"")
-;; ;;;     (sp-pair "{" nil :actions :rem)
-;;      (sp-pair "'" nil :actions :rem)
-;;      (sp-pair "`" nil :actions :rem)
-;;
-;;      ;; adds `' as a local pair in emacs-lisp-mode
-;;      (sp-local-pair 'emacs-lisp-mode "`" "'")
-;;
-;;      (sp-with-modes '(racket-mode racket-repl-mode scheme-mode lisp-mode)
-;;        (sp-local-pair "#|" "|#" :trigger "\\#|"))))
-
+(add-parens-hooks 1 1 '(lisp common-lisp slime-repl emacs-lisp ielm))
+(add-parens-hooks 1 1 '(scheme racket geiser-repl racket-repl clojure cider-repl))
+(add-parens-hooks 1 1 '(css html rust c d lua js eshell gimp markdown conf))
 
 (provide 'parens-cfg)
+ 
